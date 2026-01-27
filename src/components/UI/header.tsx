66adf7e132bfe9090454172445da5ce40a911a80
tskorohod <tskorohod@lmteam.by>
2026-01-27 22:02:01 +0300

    changed header(nav items, added favicon, logo, deleted unusing pictures)

--- src/components/UI/header.tsx
+++ src/components/UI/header.tsx
@@ -1,43 +1,57 @@
 "use client";
 
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@heroui/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const AcmeLogo = () => {
+export const Logo = () => {
 	return (
-		<svg fill="none" height="36" viewBox="0 0 32 32" width="36">
-			<path
-				clipRule="evenodd"
-				d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
-				fill="currentColor"
-				fillRule="evenodd"
-			/>
-		</svg>
+		<Image
+			src="/food-logo.png"
+			alt="Татарская кухня"
+			width={26} height={26}
+			priority
+		/>
 	);
 };
 
 export default function Header() {
+	const pathname = usePathname();
+	console.log(pathname)
+
+	const navItems = [
+		{href: "/", label: "Рецепты"},
+		{href: "/ingridiens", label: "Ингридиенты"},
+		{href: "/about", label: "О нас"}
+	]
 	return (
 		<Navbar>
 			<NavbarBrand>
-				<AcmeLogo />
-				<p className="font-bold text-inherit">ACME</p>
+				<Link href="/" className="flex gap-1">
+					<Logo />
+					<p className="font-bold text-inherit">Татарская кухня</p>
+				</Link>
 			</NavbarBrand>
 			<NavbarContent className="hidden sm:flex gap-4" justify="center">
-				<NavbarItem>
-					<Link color="foreground" href="#">
-						Features
-					</Link>
-				</NavbarItem>
-				<NavbarItem isActive>
-					<Link aria-current="page" href="#">
-						Customers
-					</Link>
-				</NavbarItem>
-				<NavbarItem>
-					<Link color="foreground" href="#">
-						Integrations
-					</Link>
-				</NavbarItem>
+				{navItems.map(item => {
+					const isActive = pathname === item.href;
+					return <NavbarItem key={item.href}>
+						<Link
+							color="foreground"
+							href={item.href}
+							className={`px-3 py-1
+								${isActive ? "text-blue-500" : "text-foreground"}
+								hover: text-blue-300 hover:border
+								hover: text-blue-300 hover:rounded-md
+								transition-colors
+								transition-border
+								duration-200`}
+							>
+							{item.label}
+						</Link>
+					</NavbarItem>
+				})}
 			</NavbarContent>
 			<NavbarContent justify="end">
 				<NavbarItem className="hidden lg:flex">
