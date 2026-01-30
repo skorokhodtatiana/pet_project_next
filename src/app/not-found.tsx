06e23a85b507aaa0767ff4c21cd5f96132ac9aaf
tskorohod <tskorohod@lmteam.by>
2026-01-30 20:19:07 +0300

    added page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,17 @@
+"use client"
+
+import { Button, Link } from "@heroui/react";
+
 const NotFound = () => {
 	return (
-		<div>
-			<h1>О нас</h1>
+		<div className="flex flex-col justify-center items-center">
+			<div className="text-8xl font-bold text-gray-300">404</div>
+
+			<h1 className="text-3xl font-bold tracking-tight">Страница не найдена</h1>
+
+			<div className="pt-6">
+				<Button as={Link} color="primary" variant="shadow" href="/">Вернуться на главную</Button>
+			</div>
 		</div>
 	)
 }
