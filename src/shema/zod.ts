cf96f848d6923c35c4303edf8535d976ae862ae8
tskorohod <tskorohod@lmteam.by>
2026-02-19 22:16:54 +0300

    added verification in zod

--- src/shema/zod.ts
+++ src/shema/zod.ts
@@ -1,10 +1,24 @@
 import { object, string } from "zod";
 
 export const signInSchema = object({
-	email: string()
+	email: string({
+		error: (issue) => {
+			if (issue.input === undefined) {
+				return "Поле email обязательно";
+			}
+			return "Некорректный тип данных";
+		},
+	})
 		.min(1, "Email is required")
 		.email("Invalid email"),
-	password: string()
+	password: string({
+		error: (issue) => {
+			if (issue.input === undefined) {
+				return "Поле password обязательно";
+			}
+			return "Некорректный тип данных";
+		},
+	})
 		.min(1, "Password is required")
 		.min(6, "Password must be more than 6 characters")
 		.max(32, "Password must be less than 32 characters"),
