9147add48e509727dfe7f91551bcfb6d1fcbc8be
tskorohod <tskorohod@lmteam.by>
2026-02-27 20:58:57 +0300

    added layout of ingridience form

--- src/app/ingridiens/page.tsx
+++ src/app/ingridiens/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 
-import PageContent from "@/src/components/common/page-content";
+//import PageContent from "@/src/components/common/page-content";
+import IngridientForm from "@/src/forms/ingridient.form";
 
 const Ingridiens = () => {
 	return (
 		<div>
-			<PageContent></PageContent>
+			{/* <PageContent></PageContent> */}
+			<IngridientForm></IngridientForm>
 		</div>
 	)
 }
--- src/forms/ingridient.form.tsx
+++ src/forms/ingridient.form.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import { Form } from "@heroui/form";
 import { Input } from "@heroui/input";
+import { Button, Select, SelectItem } from "@heroui/react";
 import { useState } from "react";
-
+import { CATEGORY_OPTIONS, UNIT_OPTIONS } from "../constans/select-options";
 
 const IngridientForm = () => {
 	const[formData, setFormData] = useState({
@@ -35,6 +38,81 @@ const IngridientForm = () => {
 					return null;
 				}}
 			/>
+			<div className="flex gap-2 w-full">
+				<div className="w-1/3">
+					<Select
+						isRequired
+						name="category"
+						placeholder="Категория"
+						aria-label="category"
+						selectedKeys={formData.category ? [formData.category] : []}
+						classNames={{
+							trigger: "bg-default-100 w-full",
+							innerWrapper: "text-sm",
+							value: "truncate",
+							selectorIcon: "text-black"
+						}}
+						onChange={(e) => setFormData({...formData, category: e.target.value})}
+					>
+						{CATEGORY_OPTIONS.map((category) => (
+							<SelectItem className="text-black" key={category.value}>{category.label}</SelectItem>
+						))}
+					</Select>
+				</div>
+				<div className="w-1/3">
+					<Select
+						isRequired
+						name="unit"
+						placeholder="Ед. изм."
+						aria-label="unit"
+						selectedKeys={formData.unit ? [formData.unit] : []}
+						classNames={{
+							trigger: "bg-default-100 w-full",
+							innerWrapper: "text-sm",
+							value: "truncate",
+							selectorIcon: "text-black"
+						}}
+						onChange={(e) => setFormData({...formData, unit: e.target.value})}
+					>
+						{UNIT_OPTIONS.map((unit) => (
+							<SelectItem className="text-black" key={unit.value}>{unit.label}</SelectItem>
+						))}
+					</Select>
+				</div>
+				<div className="w-1/3">
+					<Input
+						isRequired
+						name="pricePerUnit"
+						placeholder="Цена"
+						type="number"
+						value={
+							formData.pricePerUnit !== null
+								? formData.pricePerUnit.toString()
+								: ""
+						}
+						onChange={(e) => {
+							const value = e.target.value ? parseFloat(e.target.value) : null;
+							setFormData({...formData, pricePerUnit: value})
+						}}
+					/>
+				</div>
+			</div>
+			<Input
+				name="description"
+				value={formData.description}
+				type="text"
+				placeholder="Введите описание(необязательно)"
+				classNames={{
+					inputWrapper:"bg-default-100",
+					input:"text-sm focus:outline-none"
+				}}
+				onChange={(e) => setFormData({...formData, description: e.target.value})}
+			/>
+			<div className="flex w-full items-center justify-end">
+				<Button className="flex w-full items-center justify-end">
+					Добавить ингридиент
+				</Button>
+			</div>
 		</Form>
 	)
 }
