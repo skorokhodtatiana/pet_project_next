4072ec33b43d40f9995598f4bdc851b07226abfc
tskorohod <tskorohod@lmteam.by>
2026-04-13 23:17:55 +0300

    changed import

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@/src/generated/prisma";
+//import { PrismaClient } from "@/src/generated/prisma";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
