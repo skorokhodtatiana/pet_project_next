be46cc7281768402fb540d5d8ac83ea3a5f8052d
tskorohod <tskorohod@lmteam.by>
2026-04-16 23:12:03 +0300

    added recipe store

---
+++ src/store/recipe.store.ts
@@ -0,0 +1,108 @@
+import { create } from "zustand";
+import { IRecipe } from "../types/recipe";
+import { createRecipe, deleteRecipes, getRecipes, updateRecipes } from "../actions/recipe";
+
+interface IActionResult {
+	success: boolean,
+	recipe?: IRecipe,
+	error?: string,
+}
+
+interface IRecipeState {
+	recipes: IRecipe[];
+	isLoading: boolean;
+	error: string | null;
+	loadingRecipes: () => Promise<void>;
+	addRecipe: (formData: FormData) => Promise<IActionResult>;
+	updateRecipe: (id: string, formData: FormData) => Promise<IActionResult>;
+	removeRecipe: (id: string) => Promise<void>;
+}
+
+export const useRecipeStore = create<IRecipeState>((set) => ({
+	recipes: [],
+	isLoading: false,
+	error: null,
+	loadingRecipes: async() => {
+		set({isLoading: true, error: null});
+
+		try {
+			const result = await getRecipes();
+
+			if (result.success) {
+				set({recipes: result.recipes, isLoading: false})
+			} else {
+				set({error: result.error, isLoading: false})
+			}
+		} catch (error) {
+			console.log('error', error);
+			set({error: 'Ошибка при загрузке рецептов', isLoading: false});
+		}
+	},
+	addRecipe: async(formData: FormData) => {
+		set({error: null});
+
+		try {
+			const result = await createRecipe(formData);
+
+			if (result.success) {
+				set((state) => ({
+					resipes: [...state.recipes, result.recipe],
+					isLoading: false
+				}))
+				return {success: true, recipe: result.recipe!}
+			} else {
+				set({error: result.error, isLoading: false});
+				return {success: false, error: result.error}
+			}
+		} catch (error) {
+			console.log('error', error);
+			set({error: 'Ошибка при добавлении рецепта', isLoading: false});
+			return {success: false, error: "Ошибка при добавлении рецепта"};
+		}
+	},
+	updateRecipe: async(id: string, formData: FormData) => {
+		set({error: null})
+
+		try {
+			const result = await updateRecipes(id, formData);
+
+			if (result.success) {
+				set((state) => ({
+					recipes: state.recipes.map(recipe => (
+						recipe.id === id ? result.recipe! : recipe
+					)),
+					isLoading: false
+				}));
+				return {success: true, recipe: result.recipe}
+			} else {
+				set({error: "Ошибка при обновлении рецепта", isLoading: false});
+				return {success: false, error: "Ошибка при обновлении рецепта"}
+			}
+		} catch (error) {
+			console.log('error', error);
+			set({error: 'Ошибка при обновлении рецепта', isLoading: false});
+			return {success: false, error: "Ошибка при обновлении рецепта"};
+		}
+	},
+	removeRecipe: async (id: string) => {
+		set({error: null})
+
+		try {
+			const result = await deleteRecipes(id);
+
+			if (result.success) {
+				set((state) => ({
+					recipes: state.recipes.filter(recipe => (
+						recipe.id !== id
+					)),
+					isLoading: false
+				}));
+			} else {
+				set({error: result.error, isLoading: false});
+			}
+		} catch (error) {
+			console.log('error', error);
+			set({error: 'Ошибка при удалении рецепта', isLoading: false});
+		}
+	}
+}))
