45ab4248507f45c4133d1747db63e09b2359f517
tskorohod <tskorohod@lmteam.by>
2026-04-23 13:25:49 +0300

    added loading for recipe

--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -4,25 +4,31 @@ import { useSession } from "next-auth/react";
 import { useAuthStore } from "../store/auth.store";
 import { useEffect } from "react";
 import { useIngridientStore } from "../store/ingridient.store";
+import { useRecipeStore } from "../store/recipe.store";
 
 interface IProps {
 	children: React.ReactNode;
 }
 
 const AppLoader = ({children}: IProps) => {
-	const {data: session, status} = useSession();
-	const {isAuth, setAuthState} = useAuthStore();
-	const {loadIngridient} = useIngridientStore();
+	const { data: session, status } = useSession();
+	const { isAuth, setAuthState } = useAuthStore();
+	const { loadIngridient } = useIngridientStore();
+	const { loadingRecipes } = useRecipeStore();
 
 	useEffect(() => {
 		setAuthState(status, session);
-	},[status, session, setAuthState])
+	},[status, session, setAuthState]);
 
 	useEffect(() => {
 		if (isAuth) {
 			loadIngridient();
 		}
-	}, [isAuth, loadIngridient])
+	}, [isAuth, loadIngridient]);
+
+	useEffect(() => {
+		loadingRecipes();
+	}, [loadingRecipes]);
 
 	return(
 		<>{children}</>
