70f6cf4b0205d9ad47f072c08d50d0415b4ba995
tskorohod <tskorohod@lmteam.by>
2026-04-24 21:22:07 +0300

    added home page

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import Link from "next/link";
+import { useRecipeStore } from "../store/recipe.store";
+import { Button } from "@heroui/react";
+import RecipeCard from "../components/common/recipe-card";
+
+export default function Home() {
+	const { recipes, isLoading, error } = useRecipeStore();
+
+	return (
+		<>
+			<div className="flex w-full justify-center items-center mb-4">
+				<Link href="/recipes/new">
+					<Button color="primary">Добавить рецепт</Button>
+				</Link>
+			</div>
+
+			{error && <p className="text-red-500 mt-4">{error}</p>}
+
+			{isLoading && <p>...Загрузка</p>}
+
+			{recipes.map(recipe => (
+				<RecipeCard key={recipe.id} recipe={recipe}/>
+			))}
+		</>
+	);
+}
